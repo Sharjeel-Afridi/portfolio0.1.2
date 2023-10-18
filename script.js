17fcca10af2b583f74b502d69f228226fc1152dc
Sharjeel-Afridi <afridisharjeel8@gmail.com>
2023-10-18 22:32:43 +0530

    scroll feature

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const resumeBtn = document.getElementById('resume-btn');
-const url = 'https://drive.google.com/file/d/1A3CDfTW4-Py7Z4Uo94u3G5fHtTZGh94Q/view?usp=sharing'
-resumeBtn.addEventListener('click', function (){
+const url = 'https://drive.google.com/file/d/1A3CDfTW4-Py7Z4Uo94u3G5fHtTZGh94Q/view?usp=sharing';
+const aboutEl = document.getElementById('about')
+
+function resume(){
     window.open(url, '_blank');
-})
+}
+
+function scrollTo(className){
+    const target = document.querySelector(`.${className}`); 
+    if(target){
+        target.scrollIntoView({
+            behaviour: 'smooth'
+        })
+    }
+}
+aboutEl.addEventListener('click', () => {
+    scrollTo('about')
+});
+resumeBtn.addEventListener('click', resume);
+
